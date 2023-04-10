3eeffdb647ae376e8ff8b62ea68ee0dcd04302c1
aadil42 <adilkelawala@gmail.com>
2023-04-10 14:07:09 +0530

    displaying components in main App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MainHeader from './Components/MainHeader/MainHeader';
+import  Login from './Components/Login/Login';
+import Home from './Components/Home/Home';
+
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+
   return (
-    <div>
-      <MainHeader />
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+      {!isLoggedIn && <Login onLogin={loginHandler} />}
+      {isLoggedIn && <Home onLogout={logoutHandler} />}      
+      </main>
+    </React.Fragment>
   );
 }
 
