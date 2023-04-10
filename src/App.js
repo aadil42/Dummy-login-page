328e884c0377519738382ee11a4e3d244970ac70
aadil42 <adilkelawala@gmail.com>
2023-04-10 14:02:06 +0530

    rendering MainHeader component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+import MainHeader from './Components/MainHeader/MainHeader';
 function App() {
   return (
     <div>
-      myApp
+      <MainHeader />
     </div>
   );
 }
