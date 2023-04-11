f49819b49a6b3c23312f7d0b0f6510a9a97127d4
aadil42 <adilkelawala@gmail.com>
2023-04-11 14:47:45 +0530

    storing user's login status in browser and keeping him logged in on a refresh. All this is possible because of the useEffect, if you try to store localstorge without useEffect it will be an infinite loop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MainHeader from './Components/MainHeader/MainHeader';
 import  Login from './Components/Login/Login';
@@ -7,15 +7,27 @@ import Home from './Components/Home/Home';
 function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  const userLoggedInStatsInBrowser = localStorage.getItem('isLoggedIn');
+  
+  useEffect(() => {
+    if(localStorage.getItem('isLoggedIn') === '1') {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [userLoggedInStatsInBrowser]);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', '1');
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.setItem('isLoggedIn', '0');
   };
 
 
